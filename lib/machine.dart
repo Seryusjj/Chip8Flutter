d63430d4eb8d1c783ceed6fff135d5b59ea0dc95
Sergio Jiménez Jiménez <sergiojj932@gmail.com>
2020-09-02 22:57:58 +0200

    Separate instructions from machine

--- lib/machine.dart
+++ lib/machine.dart
@@ -4,6 +4,8 @@ import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 
+import 'machine_operations.dart';
+
 class OpCode {
   Uint16List _opCodeStore;
 
@@ -70,219 +72,10 @@ enum Status {
   Running,
 }
 
-typedef OpCall = void Function(Machine mac, OpCode code);
 
-Map<int, OpCall> operations = {
-  0: (Machine mac, OpCode code) => {
-        if (code.y == 0xC)
-          {
-            // super chip extension
-            mac.missing(code)
-          }
-        else
-          kk_SubOperation[code.kk](mac, code)
-      },
-  1: (Machine mac, OpCode code) => mac.jp(code),
-  2: (Machine mac, OpCode code) => mac.call(code),
-  3: (Machine mac, OpCode code) => mac.se_xkk(code),
-  4: (Machine mac, OpCode code) => mac.sne_xkk(code),
-  5: (Machine mac, OpCode code) => mac.se_xy(code),
-  6: (Machine mac, OpCode code) => mac.ld_xkk(code),
-  7: (Machine mac, OpCode code) => mac.add_xkk(code),
-  8: (Machine mac, OpCode code) => n_SubOperation[code.n](mac, code),
-  9: (Machine mac, OpCode code) => mac.sne_xy(code),
-  0xA: (Machine mac, OpCode code) => mac.ld_innn(code),
-  0xB: (Machine mac, OpCode code) => mac.jp_v0nnn(code),
-  0xC: (Machine mac, OpCode code) => mac.rnd_xkk(code),
-  0xD: (Machine mac, OpCode code) => mac.drw_xyn(code),
-  0xE: (Machine mac, OpCode code) => kk_SubOperation[code.kk](mac, code),
-  0xF: (Machine mac, OpCode code) => kk_SubOperation[code.kk](mac, code),
-};
-
-Map<int, OpCall> n_SubOperation = {
-  0: (Machine mac, OpCode code) => mac.ld_xy(code),
-  1: (Machine mac, OpCode code) => mac.or_xy(code),
-  2: (Machine mac, OpCode code) => mac.and_xy(code),
-  3: (Machine mac, OpCode code) => mac.xor_xy(code),
-  4: (Machine mac, OpCode code) => mac.add_xy(code),
-  5: (Machine mac, OpCode code) => mac.sub_xy(code),
-  6: (Machine mac, OpCode code) => mac.shr_x(code),
-  7: (Machine mac, OpCode code) => mac.subn_xy(code),
-  0xE: (Machine mac, OpCode code) => mac.shl_x(code),
-};
-
-Map<int, OpCall> kk_SubOperation = {
-  0x9E: (Machine mac, OpCode code) => mac.skp_x(code),
-  0xA1: (Machine mac, OpCode code) => mac.sknp_x(code),
-  0x07: (Machine mac, OpCode code) => mac.ld_xdt(code),
-  0x0A: (Machine mac, OpCode code) => mac.ld_xk(code),
-  0x15: (Machine mac, OpCode code) => mac.ld_dtx(code),
-  0x18: (Machine mac, OpCode code) => mac.ld_stx(code),
-  0x1E: (Machine mac, OpCode code) => mac.add_ix(code),
-  0x29: (Machine mac, OpCode code) => mac.ld_fx(code),
-  0x33: (Machine mac, OpCode code) => mac.ld_bx(code),
-  0x55: (Machine mac, OpCode code) => mac.ld_ix(code),
-  0x65: (Machine mac, OpCode code) => mac.ld_xi(code),
-  0xE0: (Machine mac, OpCode code) => mac.cls(code),
-  0xEE: (Machine mac, OpCode code) => mac.ret(code),
-  // super chip extension 0x0
-  0xFB: (Machine mac, OpCode code) => mac.missing(code),
-  0xFC: (Machine mac, OpCode code) => mac.missing(code),
-  0xFD: (Machine mac, OpCode code) => mac.missing(code),
-  0xFE: (Machine mac, OpCode code) => mac.missing(code),
-  0xFF: (Machine mac, OpCode code) => mac.missing(code),
-  // super chip extension 0xF
-  0x30: (Machine mac, OpCode code) => mac.missing(code),
-  0x75: (Machine mac, OpCode code) => mac.missing(code),
-  0x85: (Machine mac, OpCode code) => mac.missing(code),
-};
 
 class Machine {
-  missing(OpCode op) {
-    port.send("not implemented op: ${op.value.toRadixString(16)}");
-  }
 
-  cls(OpCode op) {
-    port.send("CLS");
-  }
-
-  ret(OpCode op) {
-    port.send("RET");
-  }
-
-  jp(OpCode op) {
-    port.send("JP ${op.nnn.toRadixString(16)}");
-  }
-
-  call(OpCode op) {
-    port.send("CALL ${op.nnn.toRadixString(16)}");
-  }
-
-  se_xkk(OpCode op) {
-    port.send("SE ${op.x.toRadixString(16)}, ${op.kk.toRadixString(16)}");
-  }
-
-  sne_xkk(OpCode op) {
-    port.send("SNE ${op.x.toRadixString(16)}, ${op.kk.toRadixString(16)}");
-  }
-
-  se_xy(OpCode op) {
-    port.send("SE ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
-  }
-
-  ld_xkk(OpCode op) {
-    port.send("LD ${op.x.toRadixString(16)}, ${op.kk.toRadixString(16)}");
-  }
-
-  add_xkk(OpCode op) {
-    port.send("ADD ${op.x.toRadixString(16)}, ${op.kk.toRadixString(16)}");
-  }
-
-  ld_xy(OpCode op) {
-    port.send("LD ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
-  }
-
-  or_xy(OpCode op) {
-    port.send("OR ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
-  }
-
-  and_xy(OpCode op) {
-    port.send("AND ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
-  }
-
-  xor_xy(OpCode op) {
-    port.send("XOR ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
-  }
-
-  add_xy(OpCode op) {
-    port.send("ADD ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
-  }
-
-  sub_xy(OpCode op) {
-    port.send("SUB ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
-  }
-
-  shr_x(OpCode op) {
-    port.send("SHR ${op.x.toRadixString(16)}");
-  }
-
-  subn_xy(OpCode op) {
-    port.send("SUBN ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
-  }
-
-  shl_x(OpCode op) {
-    port.send("SHL ${op.x.toRadixString(16)}");
-  }
-
-  sne_xy(OpCode op) {
-    port.send("SNE ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
-  }
-
-  ld_innn(OpCode op) {
-    port.send("LD I, ${op.nnn.toRadixString(16)}");
-  }
-
-  jp_v0nnn(OpCode op) {
-    port.send("JP V0, ${op.nnn.toRadixString(16)}");
-  }
-
-  rnd_xkk(OpCode op) {
-    port.send("RND ${op.x.toRadixString(16)}, ${op.kk.toRadixString(16)}");
-  }
-
-  drw_xyn(OpCode op) {
-    // super chip extension
-    if (op.n == 0) {
-      port.send("DRW ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}, 0");
-    } else {
-      port.send(
-          "DRW ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}, ${op.n.toRadixString(16)}");
-    }
-  }
-
-  skp_x(OpCode op) {
-    port.send("SKP ${op.x.toRadixString(16)}");
-  }
-
-  sknp_x(OpCode op) {
-    port.send("SKNP ${op.x.toRadixString(16)}");
-  }
-
-  ld_xdt(OpCode op) {
-    port.send("LD ${op.x.toRadixString(16)}, DT");
-  }
-
-  ld_xk(OpCode op) {
-    port.send("LD ${op.x.toRadixString(16)}, K");
-  }
-
-  ld_dtx(OpCode op) {
-    port.send("LD DT, ${op.x.toRadixString(16)}");
-  }
-
-  ld_stx(OpCode op) {
-    port.send("LD ST, ${op.x.toRadixString(16)}");
-  }
-
-  add_ix(OpCode op) {
-    port.send("ADD I, ${op.x.toRadixString(16)}");
-  }
-
-  ld_fx(OpCode op) {
-    port.send("LD F, ${op.x.toRadixString(16)}");
-  }
-
-  ld_bx(OpCode op) {
-    port.send("LD B, ${op.x.toRadixString(16)}");
-  }
-
-  ld_ix(OpCode op) {
-    port.send("LD [I], ${op.x.toRadixString(16)}");
-  }
-
-  ld_xi(OpCode op) {
-    port.send("LD ${op.x.toRadixString(16)}, [I]");
-  }
 
   Uint8List mem; // cpu available memory
   Uint16List _pc_sp_i;
@@ -392,17 +185,15 @@ class Machine {
       // opcodes are made of 16 bits, memory is made of 8,
       // so two mem entries = 1 opcode
       op.value = mem[pc] << 8 | mem[++pc];
-      opcodes += op.value.toRadixString(16);
+      // opcodes += op.value.toRadixString(16);
       pc++;
 
       if (op != null && op.value != 0) {
         var opFunction = operations[op.p];
-        if (opFunction != null) {
-          opFunction(this, op);
-        }
+        // opFunction will be null of operation not found
+        // so an exception will raise that is ok
+        opFunction(this, op);
       }
-      // search for the op and exec it
-
     }
 
     port.send(opcodes);
---
+++ lib/machine_operations.dart
@@ -0,0 +1,215 @@
+
+import 'machine.dart';
+
+typedef OpCall = void Function(Machine mac, OpCode code);
+
+/// The only element publicly exposed is this operations map
+Map<int, OpCall> operations = {
+  0: (Machine mac, OpCode code) => {
+    // super chip extension
+    if (code.y == 0xC)
+      {_missing}
+    else
+      _kk_SubOperation[code.kk]
+  },
+  1: _jp,
+  2: _call,
+  3: _se_xkk,
+  4: _sne_xkk,
+  5: _se_xy,
+  6: _ld_xkk,
+  7: _add_xkk,
+  8: (Machine mac, OpCode code) => _n_SubOperation[code.n],
+  9: _sne_xy,
+  0xA: _ld_innn,
+  0xB: _jp_v0nnn,
+  0xC: _rnd_xkk,
+  0xD: _drw_xyn,
+  0xE: (Machine mac, OpCode code) => _kk_SubOperation[code.kk],
+  0xF: (Machine mac, OpCode code) => _kk_SubOperation[code.kk],
+};
+
+Map<int, OpCall> _n_SubOperation = {
+  0: _ld_xy,
+  1: _or_xy,
+  2: _and_xy,
+  3: _xor_xy,
+  4: _add_xy,
+  5: _sub_xy,
+  6: _shr_x,
+  7: _subn_xy,
+  0xE: _shl_x,
+};
+
+Map<int, OpCall> _kk_SubOperation = {
+  0x9E: _skp_x,
+  0xA1: _sknp_x,
+  0x07: _ld_xdt,
+  0x0A: _ld_xk,
+  0x15: _ld_dtx,
+  0x18: _ld_stx,
+  0x1E: _add_ix,
+  0x29: _ld_fx,
+  0x33: _ld_bx,
+  0x55: _ld_ix,
+  0x65: _ld_xi,
+  0xE0: _cls,
+  0xEE: _ret,
+  // super chip extension 0x0
+  0xFB: _missing,
+  0xFC: _missing,
+  0xFD: _missing,
+  0xFE: _missing,
+  0xFF: _missing,
+  // super chip extension 0xF
+  0x30: _missing,
+  0x75: _missing,
+  0x85: _missing,
+};
+
+
+_missing(Machine mac, OpCode op) {
+  throw Exception('Unimplemented operation ${op.value.toRadixString(16)}');
+}
+
+_cls(Machine mac, OpCode op) {
+  mac.port.send("CLS");
+}
+
+_ret(Machine mac, OpCode op) {
+  mac.port.send("RET");
+}
+
+_jp(Machine mac,OpCode op) {
+  mac.port.send("JP ${op.nnn.toRadixString(16)}");
+}
+
+_call(Machine mac, OpCode op) {
+  mac.port.send("CALL ${op.nnn.toRadixString(16)}");
+}
+
+_se_xkk(Machine mac, OpCode op) {
+  mac.port.send("SE ${op.x.toRadixString(16)}, ${op.kk.toRadixString(16)}");
+}
+
+_sne_xkk(Machine mac, OpCode op) {
+  mac.port.send("SNE ${op.x.toRadixString(16)}, ${op.kk.toRadixString(16)}");
+}
+
+_se_xy(Machine mac, OpCode op) {
+  mac.port.send("SE ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
+}
+
+_ld_xkk(Machine mac, OpCode op) {
+  mac.port.send("LD ${op.x.toRadixString(16)}, ${op.kk.toRadixString(16)}");
+}
+
+_add_xkk(Machine mac, OpCode op) {
+  mac.port.send("ADD ${op.x.toRadixString(16)}, ${op.kk.toRadixString(16)}");
+}
+
+_ld_xy(Machine mac, OpCode op) {
+  mac.port.send("LD ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
+}
+
+_or_xy(Machine mac, OpCode op) {
+  mac.port.send("OR ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
+}
+
+_and_xy(Machine mac, OpCode op) {
+  mac.port.send("AND ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
+}
+
+_xor_xy(Machine mac, OpCode op) {
+  mac.port.send("XOR ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
+}
+
+_add_xy(Machine mac, OpCode op) {
+  mac.port.send("ADD ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
+}
+
+_sub_xy(Machine mac, OpCode op) {
+  mac.port.send("SUB ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
+}
+
+_shr_x(Machine mac, OpCode op) {
+  mac.port.send("SHR ${op.x.toRadixString(16)}");
+}
+
+_subn_xy(Machine mac, OpCode op) {
+  mac.port.send("SUBN ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
+}
+
+_shl_x(Machine mac, OpCode op) {
+  mac.port.send("SHL ${op.x.toRadixString(16)}");
+}
+
+_sne_xy(Machine mac, OpCode op) {
+  mac.port.send("SNE ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}");
+}
+
+_ld_innn(Machine mac, OpCode op) {
+  mac.port.send("LD I, ${op.nnn.toRadixString(16)}");
+}
+
+_jp_v0nnn(Machine mac, OpCode op) {
+  mac.port.send("JP V0, ${op.nnn.toRadixString(16)}");
+}
+
+_rnd_xkk(Machine mac, OpCode op) {
+  mac.port.send("RND ${op.x.toRadixString(16)}, ${op.kk.toRadixString(16)}");
+}
+
+_drw_xyn(Machine mac, OpCode op) {
+  // super chip extension
+  if (op.n == 0) {
+    mac.port.send("DRW ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}, 0");
+  } else {
+    mac.port.send(
+        "DRW ${op.x.toRadixString(16)}, ${op.y.toRadixString(16)}, ${op.n.toRadixString(16)}");
+  }
+}
+
+_skp_x(Machine mac, OpCode op) {
+  mac.port.send("SKP ${op.x.toRadixString(16)}");
+}
+
+_sknp_x(Machine mac, OpCode op) {
+  mac.port.send("SKNP ${op.x.toRadixString(16)}");
+}
+
+_ld_xdt(Machine mac, OpCode op) {
+  mac.port.send("LD ${op.x.toRadixString(16)}, DT");
+}
+
+_ld_xk(Machine mac, OpCode op) {
+  mac.port.send("LD ${op.x.toRadixString(16)}, K");
+}
+
+_ld_dtx(Machine mac, OpCode op) {
+  mac.port.send("LD DT, ${op.x.toRadixString(16)}");
+}
+
+_ld_stx(Machine mac, OpCode op) {
+  mac.port.send("LD ST, ${op.x.toRadixString(16)}");
+}
+
+_add_ix(Machine mac, OpCode op) {
+  mac.port.send("ADD I, ${op.x.toRadixString(16)}");
+}
+
+_ld_fx(Machine mac, OpCode op) {
+  mac.port.send("LD F, ${op.x.toRadixString(16)}");
+}
+
+_ld_bx(Machine mac, OpCode op) {
+  mac.port.send("LD B, ${op.x.toRadixString(16)}");
+}
+
+_ld_ix(Machine mac, OpCode op) {
+  mac.port.send("LD [I], ${op.x.toRadixString(16)}");
+}
+
+_ld_xi(Machine mac, OpCode op) {
+  mac.port.send("LD ${op.x.toRadixString(16)}, [I]");
+}
